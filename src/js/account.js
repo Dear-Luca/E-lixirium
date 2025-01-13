630291cebe67051fd794a5d2f70427e3a9fc4ee5
Luca Marchi <luca1010.marchi@gmail.com>
2025-01-13 18:22:20 +0100

    added eye to show and hide password, made the page mobile first

--- src/js/account.js
+++ src/js/account.js
@@ -3,7 +3,7 @@ document.querySelector(".btn-primary").addEventListener("click", function () {
     document.querySelectorAll("input[type='text'], input[type='email'], input[type='date'], input[type='password']").forEach(input => {
         input.disabled = false;
     });
-    
+
     document.querySelector("div section form div:nth-child(8)").style.display = "block";
     // show buttons save and cancel
     document.querySelector(".btn-success").style.display = "block";
@@ -27,5 +27,34 @@ document.querySelector(".btn-danger").addEventListener("click", function () {
     document.querySelector(".btn-primary").style.display = "block";
     document.querySelector(".btn-success").style.display = "none";
     document.querySelector(".btn-danger").style.display = "none";
+});
+
+function togglePasswordVisibility(passwordSelector, imageSelector) {
+    var password = document.querySelector(passwordSelector);
+    var image = document.querySelector(imageSelector);
+    var currentUrl = window.location.href;
+    var baseUrl = currentUrl.split('?')[0];
+
+    if (password.type === "password") {
+        password.type = "text";
+        var newUrl = baseUrl + "upload/open-eye.svg";
+        image.src = newUrl;
+        image.alt = "show password";
+    } else {
+        password.type = "password";
+        var newUrl = baseUrl + "upload/closed-eye.svg";
+        image.src = newUrl;
+        image.alt = "hide password";
+    }
+}
+
+document.querySelector("div.container div.row section.col-12 form div div:nth-child(7) button").addEventListener('click', function () {
+    togglePasswordVisibility("div.container div.row section.col-12 form div div:nth-child(7) input", "div.container div.row div.col-12.col-md-6 div.card button img:nth-child(1)");
+
+});
+
+document.querySelector("div.container div.row section.col-12 form div div:nth-child(8) button").addEventListener('click', function () {
+    togglePasswordVisibility("div.container div.row section.col-12 form div div:nth-child(8) input", "div.container div.row div.col-12.col-md-6 div.card button img:nth-child(2)");
+
 });
 
--- src/template/account-user.php
+++ src/template/account-user.php
@@ -1,5 +1,4 @@
 <?php $templateParams["js"][] = "account.js"; ?>
-
 <div class="container">
     <?php if (isset($templateParams["error"])): ?>
         <div class="alert alert-warning alert-dismissible fade show" role="alert">
@@ -8,97 +7,120 @@
         </div>
     <?php endif; ?>
     <div class="row">
-        <section class="col-3">
+        <section class="col-12 col-md-3 mb-3">
             <h2><?php echo $templateParams['userInfo'][0]['name']; ?></h2>
             <p class="h3"><?php echo $templateParams['userInfo'][0]['email']; ?></p>
         </section>
-        <section class="col-9">
+        <section class="col-12 col-md-9">
             <h2>Personal Information</h2>
             <form method="POST" action="?page=account">
                 <div class="row">
                     <!-- Card 1 -->
-                    <div class="col-md-6">
-                        <div class="card mb-3 bg-purple-mid">
+                    <div class="col-12 col-md-6 mb-3">
+                        <div class="card bg-purple-mid">
                             <label class="card-header form-label" for="name">Name:</label>
-                            <input class="card-body form-control" type="text" id="name" name="name"
-                                value="<?php echo $templateParams['userInfo'][0]['name']; ?>"
-                                data-original-value="<?php echo $templateParams['userInfo'][0]['name']; ?>" disabled />
+                            <div class="input-group card-body">
+                                <input class="form-control" type="text" id="name" name="name"
+                                    value="<?php echo $templateParams['userInfo'][0]['name']; ?>"
+                                    data-original-value="<?php echo $templateParams['userInfo'][0]['name']; ?>"
+                                    disabled />
+                            </div>
                         </div>
                     </div>
                     <!-- Card 2 -->
-                    <div class="col-md-6">
-                        <div class="card mb-3 bg-purple-mid">
+                    <div class="col-12 col-md-6 mb-3">
+                        <div class="card bg-purple-mid">
                             <label class="card-header form-label" for="surname">Surname:</label>
-                            <input class="card-body form-control" type="text" id="surname" name="surname"
-                                value="<?php echo $templateParams['userInfo'][0]['surname']; ?>"
-                                data-original-value="<?php echo $templateParams['userInfo'][0]['surname']; ?>"
-                                disabled />
+                            <div class="input-group card-body">
+                                <input class="form-control" type="text" id="surname" name="surname"
+                                    value="<?php echo $templateParams['userInfo'][0]['surname']; ?>"
+                                    data-original-value="<?php echo $templateParams['userInfo'][0]['surname']; ?>"
+                                    disabled />
+                            </div>
                         </div>
                     </div>
                     <!-- Card 3 -->
-                    <div class="col-md-6">
-                        <div class="card mb-3 bg-purple-mid">
-                            <label class="card-header form-label" for="username">Userame:</label>
-                            <input class="card-body form-control" type="text" id="username" name="username"
-                                value="<?php echo $templateParams['userInfo'][0]['username']; ?>"
-                                data-original-value="<?php echo $templateParams['userInfo'][0]['username']; ?>"
-                                disabled />
+                    <div class="col-12 col-md-6 mb-3">
+                        <div class="card bg-purple-mid">
+                            <label class="card-header form-label" for="username">Username:</label>
+                            <div class="input-group card-body">
+                                <input class="form-control" type="text" id="username" name="username"
+                                    value="<?php echo $templateParams['userInfo'][0]['username']; ?>"
+                                    data-original-value="<?php echo $templateParams['userInfo'][0]['username']; ?>"
+                                    disabled />
+                            </div>
                         </div>
                     </div>
                     <!-- Card 4 -->
-                    <div class="col-md-6">
-                        <div class="card mb-3 bg-purple-mid">
+                    <div class="col-12 col-md-6 mb-3">
+                        <div class="card bg-purple-mid">
                             <label class="card-header form-label" for="email">E-mail:</label>
-                            <input class="card-body form-control" type="text" id="email" name="email"
-                                value="<?php echo $templateParams['userInfo'][0]['email']; ?>"
-                                data-original-value="<?php echo $templateParams['userInfo'][0]['email']; ?>" disabled />
+                            <div class="input-group card-body">
+                                <input class="form-control" type="text" id="email" name="email"
+                                    value="<?php echo $templateParams['userInfo'][0]['email']; ?>"
+                                    data-original-value="<?php echo $templateParams['userInfo'][0]['email']; ?>"
+                                    disabled />
+                            </div>
                         </div>
                     </div>
                     <!-- Card 5 -->
-                    <div class="col-md-6">
-                        <div class="card mb-3 bg-purple-mid">
+                    <div class="col-12 col-md-6 mb-3">
+                        <div class="card bg-purple-mid">
                             <label class="card-header form-label" for="birthday">Birthday:</label>
-                            <input class="card-body form-control" type="date" id="birthday" name="birthday"
-                                value="<?php echo $templateParams['userInfo'][0]['birthday']; ?>"
-                                data-original-value="<?php echo $templateParams['userInfo'][0]['birthday']; ?>"
-                                disabled />
+                            <div class="input-group card-body">
+                                <input class="form-control" type="date" id="birthday" name="birthday"
+                                    value="<?php echo $templateParams['userInfo'][0]['birthday']; ?>"
+                                    data-original-value="<?php echo $templateParams['userInfo'][0]['birthday']; ?>"
+                                    disabled />
+                            </div>
                         </div>
                     </div>
                     <!-- Card 6 -->
-                    <div class="col-md-6">
-                        <div class="card mb-3 bg-purple-mid">
+                    <div class="col-12 col-md-6 mb-3">
+                        <div class="card bg-purple-mid">
                             <label class="card-header form-label" for="card_number">Card number:</label>
-                            <input class="card-body form-control" type="text" id="card_number" name="card_number"
-                                value="<?php echo $templateParams['userInfo'][0]['card_number']; ?>"
-                                data-original-value="<?php echo $templateParams['userInfo'][0]['card_number']; ?>"
-                                disabled />
+                            <div class="input-group card-body">
+                                <input class="form-control" type="text" id="card_number" name="card_number"
+                                    value="<?php echo $templateParams['userInfo'][0]['card_number']; ?>"
+                                    data-original-value="<?php echo $templateParams['userInfo'][0]['card_number']; ?>"
+                                    disabled />
+                            </div>
                         </div>
                     </div>
                     <!-- Card 7 -->
-                    <div class="col-md-6">
-                        <div class="card mb-3 bg-purple-mid">
+                    <div class="col-12 col-md-6 mb-3">
+                        <div class="card bg-purple-mid">
                             <label class="card-header form-label" for="password">Password:</label>
-                            <input class="card-body form-control" type="password" id="password" name="password"
-                                value="<?php echo $templateParams['userInfo'][0]['password']; ?>"
-                                data-original-value="<?php echo $templateParams['userInfo'][0]['password']; ?>"
-                                disabled />
+                            <div class="input-group card-body">
+                                <input class="form-control" type="password" id="password" name="password"
+                                    value="<?php echo $templateParams['userInfo'][0]['password']; ?>"
+                                    data-original-value="<?php echo $templateParams['userInfo'][0]['password']; ?>"
+                                    disabled />
+                                <button class="btn btn-outline-secondary" type="button" id="togglePassword">
+                                    <img src="<?php echo UPLOAD_DIR; ?>closed-eye.svg" alt="Hide password" />
+                                </button>
+                            </div>
                         </div>
                     </div>
                     <!-- Card 8 -->
-                    <div class="col-md-6">
-                        <div class="card mb-3 bg-purple-mid">
+                    <div class="col-12 col-md-6 mb-3">
+                        <div class="card bg-purple-mid">
                             <label class="card-header form-label" for="confirmPassword">Confirm password:</label>
-                            <input class="card-body form-control" type="password" id="confirmPassword"
-                                name="confirmPassword"
-                                value="<?php echo $templateParams['userInfo'][0]['password']; ?>"
-                                data-original-value="<?php echo $templateParams['userInfo'][0]['password']; ?>" />
+                            <div class="input-group card-body">
+                                <input class="form-control" type="password" id="confirmPassword" name="confirmPassword"
+                                    value="<?php echo $templateParams['userInfo'][0]['password']; ?>"
+                                    data-original-value="<?php echo $templateParams['userInfo'][0]['password']; ?>" />
+                                <button class="btn btn-outline-secondary" type="button" id="togglePassword">
+                                    <img src="<?php echo UPLOAD_DIR; ?>closed-eye.svg" alt="Show password" />
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div>
+                <div class="d-flex justify-content-between mt-3">
                     <button type="button" class="btn btn-primary bg-light-purple border-0 text-purple">Edit</button>
-                    <button type="submit" class="btn btn-success">Save</button>
-                    <button type="button" class="btn btn-danger">Cancel</button>
+                    <button type="submit" class="btn btn-success" style="display: none;">Save</button>
+                    <button type="button" class="btn btn-danger" style="display: none;">Cancel</button>
                 </div>
             </form>
         </section>

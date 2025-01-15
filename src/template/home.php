505afeaad0466ad713ed9ddb2e7cd4257389c33f
Andrea Baldazzi <andrea.baldazzi7@studio.unibo.it>
2025-01-15 11:35:58 +0100

    Improve home page, add particles toggle

--- src/template/home.php
+++ src/template/home.php
@@ -1,21 +1,22 @@
-<?php array_push($templateParams["js"], "particles.js"); ?>
+<?php array_push($templateParams["js"], "particles.js");
+array_push($templateParams["js"], "home.js"); ?>
 <!-- Image and Title -->
 <div class="container-fluid">
-    <section class="row m-0 justify-content-center text-center">
+    <section class="row my-5 justify-content-center text-center">
         <img src="<?php echo UPLOAD_DIR . "potion.jpg"; ?>" alt="Logo" class="img-fluid" />
         <h2 class="mt-3">The Best Spell Shop</h2>
     </section>
-
+    <hr>
     <!-- Categories Carousel -->
-    <section class="mt-5">
+    <section class="mb-5 mt-1">
         <h2>Random categories</h2>
-        <div class="row row-cols-2 row-cols-sm-3 row-cols-md-6 m-0">
+        <div class="row row-cols-2 row-cols-sm-3 row-cols-md-6 m-0 mt-4">
             <?php foreach ($templateParams["categories"] as $category): ?>
                 <div class="col">
                     <div class="card card-hover borders-0">
-                        <img src="<?php echo UPLOAD_DIR; ?>/potion.jpg" class="card-img-top" alt="">
+                        <img class="card-img-top randomColorImage" alt="">
                         <div class="card-body">
-                            <p class="text-nowrap text-center h-md-4 h-lg-5"><?php echo $category["name"]; ?></p>
+                            <p class="text-nowrap text-center fw-semibold"><?php echo $category["name"]; ?></p>
                             <a href="./?page=products&category=<?php echo $category["name"]; ?>" class="stretched-link"></a>
                         </div>
                     </div>
@@ -23,15 +24,15 @@
             <?php endforeach; ?>
         </div>
     </section>
-
+    <hr>
     <!-- Product List -->
-    <section class="mt-5">
+    <section class="mb-5 mt-1">
         <h2>Top products</h2>
-        <div class="row row-cols-2 row-cols-md-3 g-4 m-0">
+        <div class="row row-cols-2 row-cols-md-3 row-cols-xl-6 g-4 m-0">
             <?php foreach ($templateParams["products"] as $product): ?>
                 <div class="col">
                     <div class="card magic-card card-hover">
-                        <img src="<?php echo UPLOAD_DIR . "potion.jpg"; ?>" class="card-img-top" alt="" />
+                        <img src="<?php echo UPLOAD_DIR . $product["image_name"]; ?>" class="card-img-top" alt="" />
                         <div class="card-body">
                             <h3 class="card-title"><?php echo $product["name"]; ?></h3>
                             <span class="d-flex">
